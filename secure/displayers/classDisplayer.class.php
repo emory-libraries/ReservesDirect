aef7e4d8aebb2eb821e46e8dfb146fe470dbd9b1
jbwhite <jbwhite@03ac712c-d127-0410-97e8-dee5f8d9e391>
2005-06-01 15:53:58 +0000

    allow staff and personal copy owners reactivate personal copies

--- secure/displayers/classDisplayer.class.php
+++ secure/displayers/classDisplayer.class.php
@@ -1266,10 +1266,14 @@ class classDisplayer
 	            	
 	            		            	
 	            	echo '</td>';
-					if (!$ci->reserveList[$i]->item->isPersonalCopy())
-						echo "<td align=\"right\"><input type=\"checkbox\" name=\"carryReserve[]\" value=\"".$ci->reserveList[$i]->getReserveID()."\" checked></td></tr>";
+
+					if ($ci->reserveList[$i]->item->isPersonalCopy() && $ci->reserveList[$i]->item->privateUserID != $user->getUserID()
+						&& $user->getDefaultRole() <= $g_permission['staff'])
+					{
+						echo "<td align=\"left\" class=\"failedText\">Personal items can not be reactivated.<br>  Please contact your reserves desk for assistance.</td></tr>";						
+					}
 					else
-						echo "<td align=\"left\" class=\"failedText\">Personal items can not be reactivated.<br>  Please contact your reserves desk for assistance.</td></tr>";
+						echo "<td align=\"right\"><input type=\"checkbox\" name=\"carryReserve[]\" value=\"".$ci->reserveList[$i]->getReserveID()."\" checked></td></tr>";
 	            	
 			}
 		}
