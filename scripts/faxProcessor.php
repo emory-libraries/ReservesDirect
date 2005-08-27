10fb5841e98a1290c76531493eddd8a193fa86ba
croddy <croddy@03ac712c-d127-0410-97e8-dee5f8d9e391>
2005-08-27 01:04:07 +0000

    change logging, bump minor version

--- scripts/faxProcessor.php
+++ scripts/faxProcessor.php
@@ -9,7 +9,7 @@ and depositing them in the "incoming" directory
 
 Created by Chris Roddy (croddy@emory.edu)
 
-This file is part of ReservesDirect 2.1
+This file is part of ReservesDirect 2.2
 
 Copyright (c) 2004-2005 Emory University, Atlanta, Georgia.
 
@@ -34,75 +34,64 @@ http://www.reservesdirect.org/
 
 require_once(dirname(__FILE__) . "/../secure/config.inc.php");
 
-$log = fopen("$g_faxLog", w) || exit("Fatal error: Could not open log file '$g_faxLog' for writing");
+//$log = fopen("$g_faxLog", w) || exit("Fatal error: Could not open log file '$g_faxLog' for writing\n"); //broken on sagan for some reason, we'll print errors to stout :-(
 
 if($argv[1] == "")
-    exit("Usage: faxProcessor.php <filename.tif>");
+    exit("Usage: faxProcessor.php <filename.tif>\n");
 
 $inFile = $argv[1];
 
 $now = time(); 
 
 if (!is_executable($g_faxinfo_bin)) {
-    fwrite($log, "Processing $inFile: Cannot execute faxinfo binary '$g_faxinfo_bin'\n");
-    exit(1);
+    echo("Processing $inFile: Cannot execute faxinfo binary '$g_faxinfo_bin'\n");
 }
 
 if (!is_executable($g_fax2pdf_bin)) {
-    fwrite($log, "Processing $inFile: Cannot execute fax2pdf binary '$g_fax2pdf_bin'\n");
-    exit(1);
+    echo("Processing $inFile: Cannot execute fax2pdf binary '$g_fax2pdf_bin'\n");
 }
 
 if (!is_executable($g_gs_bin)) {
-    fwrite($log, "Processing $inFile: Cannot execute gs binary '$g_gs_bin'");
-    exit(1);
+    echo("Processing $inFile: Cannot execute gs binary '$g_gs_bin'");
 }
 
 $sender = exec("$g_faxinfo_bin $inFile | grep Sender | cut -d: -f2 | sed -e 's/ //g'");
 if ($sender == "") {
-    fwrite($log, "Processing $inFile: Sender is blank\n");
-    exit(1);
+    echo("Processing $inFile: Sender is blank\n");
 }
 
 $pages = exec("$g_faxinfo_bin $inFile | grep Pages | sed -e 's/[^0-9]//g'");
 if ($pages == "") {
-    fwrite($log, "Processing $inFile: Page count is blank\n");
-    exit(1);
+    echo("Processing $inFile: Page count is blank\n");
 }
 
 $newFile = $sender . "_" . $now . "_" . $pages;
 
 if (!exec("$g_fax2pdf_bin $inFile $g_faxDirectory/$newFile.tmp.pdf")) {
-    fwrite($log, "Processing $inFile: error executing fax2pdf\n");
-    exit(1);
+    echo("Processing $inFile: error executing fax2pdf\n");
 }
 
 if (!rename("$inFile", "$g_faxDirectory/$newFile.tif")) {
-    fwrite($log, "Processing $inFile: error moving file '$inFile' to '$g_faxDirectory'\n");
-    exit(1);
+    echo("Processing $inFile: error moving file '$inFile' to '$g_faxDirectory'\n");
 }
 
 if (!exec("$g_gs_bin -q -dNOPAUSE -sDEVICE=pdfwrite -sOutputFile=$g_faxDirectory/$newFile.pdf $g_faxCopyright $g_faxDirectory/$newFile.tmp.pdf -c quit")) {
-    fwrite($log, "Processing $inFile: error executing gs\n");
-    exit(1);
+    echo("Processing $inFile: error executing gs\n");
 }
 
 if (!unlink("$g_faxDirectory/$newFile.tmp.pdf")) {
-    fwrite($log, "Processing $inFile: error removing temporary PDF '$newFile.tmp.pdf'\n");
-    exit(1);
+    echo("Processing $inFile: error removing temporary PDF '$newFile.tmp.pdf'\n");
 }
 
 if (!chmod("$g_faxDirectory/$newFile.pdf", 0664)) {
-    fwrite($log, "Processing $inFile: Cannot change permissions of $g_faxDirectory/$newFile.pdf\n");
-    exit(1);
+    echo("Processing $inFile: Cannot change permissions of $g_faxDirectory/$newFile.pdf\n");
 }
 
 if (!chmod("$g_faxDirectory/$newFile.tif", 0664)) {
-    fwrite($log, "Processing $inFile: Cannot change permissions of $g_faxDirectory/$newFile.tif\n");
-    exit(1);
+    echo("Processing $inFile: Cannot change permissions of $g_faxDirectory/$newFile.tif\n");
 }
 
-fclose($log);
+//fclose($log);
 
 ?>
 
