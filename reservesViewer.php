e376ced3eb822486b8b473404ecf5e033d529b37
croddy <croddy@03ac712c-d127-0410-97e8-dee5f8d9e391>
2006-03-13 18:14:07 +0000

    fancy filenames

--- reservesViewer.php
+++ reservesViewer.php
@@ -68,7 +68,8 @@ http://www.reservesdirect.org/
 		
 		if(!empty($g_serverName) && (stripos($url, $g_serverName) !== false)) {	//if item URL points to local server, serve the document directly
 			if($stream = @fopen($url, r)) {	//open file for reading
-				$filename = end(split('/', $url));	//grab the filename to suggest 'save-as' name
+				//$filename = end(split('/', $url));	//grab the filename to suggest 'save-as' name
+                $filename = trim("{$item->author} {$item->title} {$item->itemID}." . end(split('\.', $url)));
 				//serve the doc			
 				header('Content-Type: '.$item->getMimeType());
 				header('Content-Disposition: inline; filename="'.$filename.'"');
