208e6585698e075c4929f7008b7295c1faaa4d22
dpantel <dpantel@03ac712c-d127-0410-97e8-dee5f8d9e391>
2006-04-20 18:18:14 +0000

    change copyright tables from myisam to innodb

--- scripts/2.2.2_schema_and_data_update.sql
+++ scripts/2.2.2_schema_and_data_update.sql
@@ -1,7 +1,7 @@
 --
 -- Modify access table to add enrollment status
 --
-ALTER TABLE `access` ADD `enrollment_status` set('AUTOFEED','APPROVED','PENDING','DENIED') NOT NULL DEFAULT 'PENDING';
+ALTER TABLE `access` ADD `enrollment_status` set('AUTOFEED','APPROVED','PENDING','DENIED') NOT NULL DEFAULT 'PENDING'
 
 --
 -- Set all current access records to approved
@@ -9,9 +9,14 @@ ALTER TABLE `access` ADD `enrollment_status` set('AUTOFEED','APPROVED','PENDING'
 UPDATE `access` SET `enrollment_status` = 'APPROVED'
 
 --
+-- Modify enrollment field in course instances
+--
+ALTER TABLE `course_instances` CHANGE `enrollment` `enrollment` SET('OPEN', 'MODERATED', 'CLOSED') NOT NULL DEFAULT 'OPEN'
+
+--
 -- Set all current classes to PUBLIC enrollment
 --
-UPDATE `course_instances` SET `enrollment` = 'PUBLIC'
+UPDATE `course_instances` SET `enrollment` = 'OPEN'
 
 
 -- ------------------------------------------
@@ -21,53 +26,58 @@ UPDATE `course_instances` SET `enrollment` = 'PUBLIC'
 --
 
 CREATE TABLE `copyright` (
-`copyright_id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`item_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `items`',
-`status` SET( 'NEW', 'PENDING', 'APPROVED', 'DENIED' ) NOT NULL DEFAULT 'NEW',
-`status_basis_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `copyright_status_bases`' ,
-`contact_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `copyright_contacts`' ,
-UNIQUE (
-`item_id`
-)
-) TYPE = MYISAM ;
+  `copyright_id` bigint(20) unsigned NOT NULL auto_increment,
+  `item_id` bigint(20) unsigned default NULL,
+  `status` set('NEW','PENDING','APPROVED','DENIED') NOT NULL default 'NEW',
+  `status_basis_id` bigint(20) unsigned default NULL,
+  `contact_id` bigint(20) unsigned default NULL,
+  PRIMARY KEY  (`copyright_id`),
+  UNIQUE KEY `item_id` (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `copyright_contacts` (
-`contact_id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`org_name` VARCHAR( 100 ) NULL ,
-`contact_name` VARCHAR( 100 ) NULL ,
-`address` VARCHAR( 255 ) NULL ,
-`phone` VARCHAR( 100 ) NULL ,
-`email` VARCHAR( 100 ) NULL ,
-`www` VARCHAR( 255 ) NULL 
-) TYPE = MYISAM ;
-
-
-CREATE TABLE `copyright_status_bases` (
-`status_basis_id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`status_type` SET( 'APPROVED', 'DENIED' ) NOT NULL DEFAULT 'APPROVED',
-`status_basis` VARCHAR( 100 ) NULL
-) TYPE = MYISAM ;
+  `contact_id` bigint(20) unsigned NOT NULL auto_increment,
+  `org_name` varchar(100) default NULL,
+  `contact_name` varchar(100) default NULL,
+  `address` varchar(255) default NULL,
+  `phone` varchar(100) default NULL,
+  `email` varchar(100) default NULL,
+  `www` varchar(255) default NULL,
+  PRIMARY KEY  (`contact_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE `copyright_files` (
-`copyright_file_id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`copyright_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `copyright`',
-`item_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `items`',
-INDEX ( `copyright_id` , `item_id` )
-) TYPE = MYISAM COMMENT = 'Links supporting copyright files to a copyright record';
+  `copyright_file_id` bigint(20) unsigned NOT NULL auto_increment,
+  `copyright_id` bigint(20) unsigned default NULL,
+  `item_id` bigint(20) unsigned default NULL,
+  PRIMARY KEY  (`copyright_file_id`),
+  KEY `copyright_id` (`copyright_id`,`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Links supporting copyright files to a copyright record';
 
 
 CREATE TABLE `copyright_log` (
-`copyright_log_id` BIGINT( 20 ) UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
-`copyright_id` BIGINT( 20 ) UNSIGNED NULL COMMENT 'foreign key -- `copyright`' ,
-`tstamp` DATETIME NOT NULL ,
-`user_id` INT( 11 ) UNSIGNED NULL COMMENT 'foreign key -- `users`' ,
-`action` VARCHAR( 20 ) NULL ,
-`details` VARCHAR( 100 ) NULL ,
-INDEX ( `copyright_id` , `user_id` , `action` )
-) TYPE = MYISAM ;
+  `copyright_log_id` bigint(20) unsigned NOT NULL auto_increment,
+  `copyright_id` bigint(20) unsigned default NULL,
+  `tstamp` datetime NOT NULL default '0000-00-00 00:00:00',
+  `user_id` int(11) unsigned default NULL,
+  `action` varchar(20) default NULL,
+  `details` varchar(100) default NULL,
+  PRIMARY KEY  (`copyright_log_id`),
+  KEY `user_id` (`user_id`,`action`),
+  KEY `copyright_id` (`copyright_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+
+CREATE TABLE `copyright_status_bases` (
+  `status_basis_id` bigint(20) unsigned NOT NULL auto_increment,
+  `status_type` set('APPROVED','DENIED') NOT NULL default 'APPROVED',
+  `status_basis` varchar(100) default NULL,
+  PRIMARY KEY  (`status_basis_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- -------------------------------------------------------
 
 --
 -- Add copyright lib id to libraries record
