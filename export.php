5b4f7e06a3752be2945ed7d345e3d82b221837c6
croddy <croddy@03ac712c-d127-0410-97e8-dee5f8d9e391>
2006-12-15 18:59:30 +0000

    fix more licenses

--- export.php
+++ export.php
@@ -1,4 +1,29 @@
 <?
+/*******************************************************************************
+export.php 
+
+Created by Jason White (jbwhite@emory.edu)
+
+This file is part of ReservesDirect
+
+Copyright (c) 2004-2006 Emory University, Atlanta, Georgia.
+
+Licensed under the ReservesDirect License, Version 1.0 (the "License");      
+you may not use this file except in compliance with the License.     
+You may obtain a copy of the full License at                              
+http://www.reservesdirect.org/licenses/LICENSE-1.0
+
+ReservesDirect is distributed in the hope that it will be useful,
+but is distributed "AS IS" and WITHOUT ANY WARRANTY, without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE, and without any warranty as to non-infringement of any third
+party's rights.  See the License for the specific language governing         
+permissions and limitations under the License.
+
+ReservesDirect is located at:
+http://www.reservesdirect.org/
+
+*******************************************************************************/
 header("Content-Type:  application/octet-stream");
 
 $link = "http://".$_SERVER['SERVER_NAME'] . ereg_replace('export.php', 'perl/reserves2.cgi', $_SERVER['PHP_SELF']) . "?ci=" . $_REQUEST['ci'];
--- secure/destroySession.inc.php
+++ secure/destroySession.inc.php
@@ -1,4 +1,29 @@
 <?
+/*******************************************************************************
+destroySession.inc.php
+
+Created by Jason White (jbwhite@emory.edu)
+
+This file is part of ReservesDirect
+
+Copyright (c) 2004-2006 Emory University, Atlanta, Georgia.
+
+Licensed under the ReservesDirect License, Version 1.0 (the "License");      
+you may not use this file except in compliance with the License.     
+You may obtain a copy of the full License at                              
+http://www.reservesdirect.org/licenses/LICENSE-1.0
+
+ReservesDirect is distributed in the hope that it will be useful,
+but is distributed "AS IS" and WITHOUT ANY WARRANTY, without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+PURPOSE, and without any warranty as to non-infringement of any third
+party's rights.  See the License for the specific language governing         
+permissions and limitations under the License.
+
+ReservesDirect is located at:
+http://www.reservesdirect.org/
+
+*******************************************************************************/
 session_start();
 $host = ".".$_SERVER['HTTP_HOST'];
 
