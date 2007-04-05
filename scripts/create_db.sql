5e1c34deef8ffcd65f231a54d59d01e91a8ee33d
croddy <croddy@03ac712c-d127-0410-97e8-dee5f8d9e391>
2007-04-05 18:02:39 +0000

    insert sample data for libraries

--- scripts/create_db.sql
+++ scripts/create_db.sql
@@ -789,6 +789,9 @@ CREATE TABLE IF NOT EXISTS `libraries` (
   PRIMARY KEY  (`library_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
+INSERT INTO `libraries` VALUES (1,'Test Library','test','TEST','TEST','http://www.example.edu','library@example.edu',1,1,1);
+
+
 -- 
 -- Dumping data for table `libraries`
 -- 
