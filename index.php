58a7352382261d11fea3c35d876034126ff8e84a
croddy <croddy@03ac712c-d127-0410-97e8-dee5f8d9e391>
2007-09-12 14:45:12 +0000

    so... we DO need this

--- index.php
+++ index.php
@@ -26,6 +26,10 @@ http://www.reservesdirect.org/
 
 *******************************************************************************/
 
+// workaround for workaround for ie's idiotic caching policy handling
+header("Cache-Control: no-cache");
+header("Pragma: no-cache");
+
 $load_start_time = time();
 require_once("secure/config.inc.php");
 require_once("secure/common.inc.php");
