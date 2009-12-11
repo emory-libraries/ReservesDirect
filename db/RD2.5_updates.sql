c9043d18e8e31fdd1c167bb02b9aaee5dd2f8aa6
rsutton <rsutton@03ac712c-d127-0410-97e8-dee5f8d9e391>
2009-12-11 22:48:57 +0000

    add new db fields to items #2

--- db/RD2.5_updates.sql
+++ db/RD2.5_updates.sql
@@ -1,2 +1,5 @@
 -- new column material_type for items
-ALTER TABLE `items` ADD `material_type` varchar(25) DEFAULT NULL;
+ALTER TABLE `items` ADD `material_type` varchar(255) DEFAULT NULL;
+ALTER TABLE `items` ADD `publisher` varchar(255) DEFAULT NULL;
+ALTER TABLE `items` ADD `total_pages_time` varchar(255) DEFAULT NULL;
+ALTER TABLE `items` ADD `availability` tinyint(1) DEFAULT NULL;
