907cbdb2b989a9da2cbaea95551082849225efb4
sconna2 <sconna2@03ac712c-d127-0410-97e8-dee5f8d9e391>
2010-04-13 11:56:18 +0000

    add alt tag to rd logo image tag. add end tag slash to img tags

--- login.php
+++ login.php
@@ -28,31 +28,31 @@ http://www.reservesdirect.org/
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-	<head>
-		<title>ReservesDirect Login</title>
-		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-		<link rel="stylesheet" href="css/standalone_login.css" type="text/css">
-		<script language="JavaScript1.2" src="secure/javascript/jsFunctions.js"></script>
-	</head>
+  <head>
+    <title>ReservesDirect Login</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+    <link rel="stylesheet" href="css/standalone_login.css" type="text/css">
+    <script language="JavaScript1.2" src="secure/javascript/jsFunctions.js"></script>
+  </head>
 
-	<body onload="focusOnForm();">
-		
+  <body onload="focusOnForm();">
+    
 <table width="600" border="0" align="center" cellpadding="10" cellspacing="0" bgcolor="#FFFFFF" class="borders2">
   <tr align="left" valign="top"> 
-    <td width="50%" align="left"><img src="images/logo-el-blue.jpg"></td>
-    <td width="50%" align="right" valign="middle"><img src="images/logo-rd-gold-book.jpg" width="145" height="100"></td>
+    <td width="50%" align="left"><img alt="reserves direct logo" src="images/logo-el-blue.jpg" /></td>
+    <td width="50%" align="right" valign="middle"><img alt="reserves direct logo" src="images/logo-rd-gold-book.jpg" width="145" height="100" /></td>
   </tr>
   <tr align="center" valign="top"> 
     <td colspan="2">
-    	<table width="100%" border="0" align="left" cellpadding="7" cellspacing="0">
+      <table width="100%" border="0" align="left" cellpadding="7" cellspacing="0">
         <tr align="center" valign="top"> 
           <td> 
           <table width="50%" border="0" cellspacing="0" cellpadding="0">
               <tr bgcolor="#003366"> 
-                <td align="left" valign="top" bgcolor="#003366"><img src="images/corner-blue-top-lt.gif" width="15" height="15"></td>
+                <td align="left" valign="top" bgcolor="#003366"><img alt="reserves direct logo corner" src="images/corner-blue-top-lt.gif" width="15" height="15" /></td>
                 <td width="100%" bgcolor="#003366"> <div align="center"><font color="#FFFFFF" face="Arial, Helvetica, sans-serif"><strong>Welcome
                     to ReservesDirect</strong></font></div></td>
-                <td align="right" valign="top" bgcolor="#003366"><img src="images/corner-blue-top-rt.gif" width="15" height="15"></td>
+                <td align="right" valign="top" bgcolor="#003366"><img alt="reserves direct logo corner" src="images/corner-blue-top-rt.gif" width="15" height="15" /></td>
               </tr>
               <tr> 
                 <td height="103" colspan="3" align="left" valign="top"> 
@@ -63,10 +63,10 @@ http://www.reservesdirect.org/
                     <td width="11%" height="10">&nbsp; </td>
                     <td width="75%">
                       <div id="loginText">
-					  <?php if($login_error): ?>
-					  	<font color="red">Invalid username or password</font>
-					  <?php endif; ?>
-					  <p><strong>Username:</strong>
+            <?php if($login_error): ?>
+              <font color="red">Invalid username or password</font>
+            <?php endif; ?>
+            <p><strong>Username:</strong>
                           <input name="username" type="text" size="15"></p> 
                           
                           <p><strong>Password:
@@ -85,23 +85,23 @@ http://www.reservesdirect.org/
                 
               </tr>
               <tr bgcolor="#003366"> 
-                <td align="left" valign="bottom"><img src="images/corner-blue-bottom-lt.gif" width="15" height="15"></td>
+                <td align="left" valign="bottom"><img alt="reserves direct logo corner" src="images/corner-blue-bottom-lt.gif" width="15" height="15" /></td>
                 <td width="100%">&nbsp;</td>
-                <td align="right" valign="bottom"><img src="images/corner-blue-bottom-rt.gif" width="15" height="15"></td>
+                <td align="right" valign="bottom"><img alt="reserves direct logo corner" src="images/corner-blue-bottom-rt.gif" width="15" height="15" /></td>
               </tr>
-            </table>       	  
+            </table>          
             </td>
           </tr>
       </table></td>
   </tr>
   <tr> 
     <td colspan="2">
-    	<p align="center" class="small">Use your Emory Network ID (netid) to login. This is the same username/password used for OPUS, PeopleSoft and Blackboard</p>
-    	<p align="center" class="small">If you are having difficulty logging in, you may need to sync your passwords
-	  at<br>
+      <p align="center" class="small">Use your Emory Network ID (netid) to login. This is the same username/password used for OPUS, PeopleSoft and Blackboard</p>
+      <p align="center" class="small">If you are having difficulty logging in, you may need to sync your passwords
+    at<br>
 
-	  <a href="https://password.service.emory.edu/">https://password.service.emory.edu/</a><br>
-	  Further help with passwords is available
+    <a href="https://password.service.emory.edu/">https://password.service.emory.edu/</a><br>
+    Further help with passwords is available
           from ITD (404-727-7777) during normal business hours Monday through
         Friday and limited hours on the weekend
     or check their web site: <a href="http://it.emory.edu/showdoc.cfm?docid=1079">http://it.emory.edu/showdoc.cfm?docid=1079</a></p>
@@ -110,5 +110,5 @@ http://www.reservesdirect.org/
   </tr>
 </table>
 
-	</body>
+  </body>
 </html>
