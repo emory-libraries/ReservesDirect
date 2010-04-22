fded42e9cf130857a0cbc30e9d05b5fe16afff60
branker <branker@03ac712c-d127-0410-97e8-dee5f8d9e391>
2010-04-22 15:11:03 +0000

    add some basic infrastructure for testing html includes and specifically main nav html

--- tests/groups/allTest.php
+++ tests/groups/allTest.php
@@ -6,12 +6,14 @@ define('RUNNER', true);
 require_once("ClassTests.php");
 require_once("ManagerTests.php");
 require_once("DisplayerTests.php");
+require_once("HtmlTests.php");
 
 $suite = new TestSuite('ReservesDirect Tests');
 
 $suite->addTestCase(new ClassGroupTest());  // classes
 $suite->addTestCase(new ManagerGroupTest());  // managers
 $suite->addTestCase(new DisplayerGroupTest());  // displayers
+$suite->addTestCase(new HtmlGroupTest());  // html
 
 
 $suite->run(new HtmlReporter());
---
+++ tests/html/test_nav.php
@@ -0,0 +1,54 @@
+<?php
+require_once('../UnitTest.php');
+
+class TestNavHtml extends UnitTest {
+
+  function setUp()
+  {
+    $this->includePath = '../../secure/html/';
+  }
+
+  function htmlOutput($path)
+  {
+    ob_start();
+    include ($this->includePath . $path);
+    $output = ob_get_contents();
+    ob_end_clean();
+    return $output;
+  }
+
+  function testBasicNav()
+  {
+    $output = $this->htmlOutput('main_nav.inc.html');
+
+    $this->assertPattern('/div/', $output,
+        "basic nav contains mainNav");
+    $this->assertNoPattern('/\bAdmin\b/', $output,
+        "basic nav doesn't contain admin");
+  }
+
+  function testStaffNav()
+  {
+    $_SESSION['userclass'] = 'staff';
+    $output = $this->htmlOutput('main_nav.inc.html');
+
+    $this->assertPattern('/<div id="mainNav">/', $output,
+        "staff nav contains mainNav");
+    $this->assertNoPattern('/\bAdmin\b/', $output,
+        "staff nav doesn't contain admin");
+  }
+
+  function testAdminNav()
+  {
+    $_SESSION['userclass'] = 'admin';
+    $output = $this->htmlOutput('main_nav.inc.html');
+
+    $this->assertPattern('/<div id="mainNav">/', $output,
+        "admin nav contains mainNav");
+    $this->assertPattern('/\bAdmin\b/', $output,
+        "admin nav contains admin");
+  }
+
+}
+
+?>
