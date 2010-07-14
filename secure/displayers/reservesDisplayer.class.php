403e5564405701b4b4241fa5c580ed2a95aafe4f
sconna2 <sconna2@03ac712c-d127-0410-97e8-dee5f8d9e391>
2010-07-14 14:06:59 +0000

    trac #188 Remove 'Add an Electronic Item' and 'Add a Digital Item' if there is no course instance defined in the 'Manage Items' tab.

--- secure/displayers/reservesDisplayer.class.php
+++ secure/displayers/reservesDisplayer.class.php
@@ -188,12 +188,16 @@ class reservesDisplayer extends noteDisplayer {
     if(!empty($_REQUEST['ci'])) {
       echo '            <li><a href="index.php?cmd=importClass&amp;new_ci='.$_REQUEST['ci'].'" align="center">Import Class</a></li>';
     }
-    if (!isset($request['ci']))
-      echo "              <li><a href=\"index.php?cmd=addDigitalItem\" align=\"center\">Add an Electronic Item</a></li>\n";
+    if (!isset($request['ci'])) {
+      // Do not display 'Add an Electronic Item' if the course instance is not defined.
+      //echo "              <li><a href=\"index.php?cmd=addDigitalItem\" align=\"center\">Add an Electronic Item</a></li>\n";
+    }
     else if ($request['ci'])
       echo "              <li><a href=\"index.php?cmd=addDigitalItem&ci=".$request['ci']."\" align=\"center\">Add an Electronic Item</a></li>\n";
-    if (!isset($request['ci']))
-      echo "              <li><a href=\"index.php?cmd=addPhysicalItem\">Add a Physical Item</a></li>\n";
+    if (!isset($request['ci'])) {
+      // Do not display 'Add an Physical Item' if the course instance is not defined.
+      //echo "              <li><a href=\"index.php?cmd=addPhysicalItem\">Add a Physical Item</a></li>\n";
+    }
     else if ($request['ci']) {
       echo "              <li><a href=\"index.php?cmd=addPhysicalItem&ci=".$request['ci']."\">Add a Physical Item</a></li>\n";
       echo "              <li><a href=\"index.php?cmd=faxReserve&ci=".$request['ci']."\">Fax a Document</a></li>\n";
