b8bd3881c3f4d5e3609093a317aa4402a4b5c2d6
Alex Thomas <athom09@emory.edu>
2012-03-28 11:45:04 -0400

    [Finishes #27124893] modified fabfile.py to work with git

--- fabfile.py
+++ fabfile.py
@@ -1,9 +1,9 @@
 import json
 from xml.etree.ElementTree import XML
-from fabric.api import env, local, prefix, put, sudo
+from fabric.api import env, local, prefix, put, sudo, task
 
 # recommended distribution command:
-#   $ fab load_config:settings.json deploy
+#   $ fab load_config:fab_config.json deploy
 #
 # where settings.json is a path to a file that looks something like this:
 #   {
@@ -13,7 +13,7 @@ from fabric.api import env, local, prefix, put, sudo
 #   }
 #
 # if the new version doesn't work, you can revert symlinks with:
-#   $ fab load_config:settings.json revert
+#   $ fab load_config:fab_config.json revert
 
 # env targets
 
@@ -27,27 +27,25 @@ _base_env()
 
 def _git_env():
     """Try to infer some env from local git checkout."""
-    env.git_rev = local('git rev-parse --short HEAD')
-    env.git_branch = local('git symbolic-ref -q HEAD')
-    evn.git_branch = env.git_branch.split('/')
+    env.git_rev = local('git rev-parse --short HEAD', capture=True)
+    env.git_branch = local('git symbolic-ref -q HEAD', capture=True)
+    env.git_branch = env.git_branch.split('/')
     env.git_branch =  env.git_branch[-1]
-    #env.git_rev_tag = '-r' + env.git_rev
-    #env.git_url = svn_info.find('entry/url').text
-    #set version from VERSION file
-    env.version =  local('cat VERSION')
-    print env
+    env.version =  local('cat VERSION', capture=True)
 
 try:
     _git_env()
+
 except:
     pass
 
 def _env_paths():
     """Set some env paths based on previously-generated env."""
-    env.build_dir = 'reserves-%(version)s%(svn_rev_tag)s' % env
-    env.tarball = 'reserves-%(version)s%(svn_rev_tag)s.tar.bz2' % env
+    env.build_dir = 'reserves-%(version)s-%(git_rev)s' % env
+    env.tarball = 'reserves-%(version)s-%(git_rev)s.tar.bz2' % env
 _env_paths()
 
+@task
 def load_config(file):
     """Load fab env variables from a local JSON file."""
     with open(file) as f:
@@ -66,17 +64,19 @@ def _sudo(*args, **kwargs):
 
 # local build functions
 
-def _fetch_source_from_svn():
-    """Fetch the source from svn to avoid accidentally including local
+def _fetch_source_from_git():
+    """Fetch the source from git to avoid accidentally including local
     changes."""
+    local('rm -rf  build dist')
     local('mkdir -p build')
     local('rm -rf build/%(build_dir)s' % env)
-    local('svn export -r %(svn_rev)s %(svn_url)s@%(svn_rev)s build/%(build_dir)s' % env)
+    local('git archive --output=build/%(build_dir)s.tar %(git_branch)s' % env)
 
 def _package_source():
     """Create a tarball of the source tree."""
     local('mkdir -p dist')
-    local('tar cjf dist/%(tarball)s -C build %(build_dir)s' % env)
+    local('bzip2  build/%(build_dir)s.tar' % env)
+    local('cp  build/%(tarball)s dist/%(tarball)s' % env)
 
 # remote functions
 
@@ -87,8 +87,9 @@ def _copy_tarball():
 
 def _extract_tarball():
     """Extract the remote source tarball in the appropriate directory."""
-    _sudo('cp /tmp/%(tarball)s %(extract_path)s/%(tarball)s' % env)
-    _sudo('tar xjf %(extract_path)s/%(tarball)s -C %(extract_path)s' % env)
+    _sudo('mv /tmp/%(tarball)s %(extract_path)s/%(tarball)s' % env)
+    _sudo('mkdir -p  %(extract_path)s/%(build_dir)s' % env)
+    _sudo('tar xjf %(extract_path)s/%(tarball)s -C %(extract_path)s/%(build_dir)s' % env)
 
 
 def _remote_config():
@@ -107,29 +108,36 @@ def _update_links():
 
 # bring it all together
 
+@task
 def deploy():
     """Deploy the application from source control to a remote server."""    
-    _fetch_source_from_svn()
-    #_package_source()
-    #_copy_tarball()
-    #_extract_tarball()
-    #_remote_config()
-    #_update_links()
+    _fetch_source_from_git()
+    _package_source()
+    _copy_tarball()
+    _extract_tarball()
+    _remote_config()
+    _update_links()
 
+@task
 def revert():
     """Back out the current version, updating remote symlinks to point back
     to the stored previous one."""
-    _sudo('[ -h %(extract_path)/previous ]' % env) # only if previous link exists
-    _sudo('rm -f %(extract_path)/current' % env)
-    _sudo('ln -s $(readlink %(extract_path)/previous) %(extract_path)/current' % env)
-    _sudo('rm %(extract_path)/previous' % env)
+    _sudo('[ -h %(extract_path)s/previous ]' % env) # only if previous link exists
+    _sudo('rm -f %(extract_path)s/current' % env)
+    _sudo('ln -s $(readlink %(extract_path)s/previous) %(extract_path)s/current' % env)
+    _sudo('rm %(extract_path)s/previous' % env)
     
+@task
 def clean_local():
     """Remove local files created during deployment."""
     local('rm -rf dist build')
     
+@task
 def get_latest_commit():
-    """Run 'svn up' to get the latest commit."""    
-    local('svn up') 
-    _svn_env()
-    _env_paths()
+    """Run 'git update' to get the latest commit."""    
+    try:
+        local('git pull --rebase') 
+        _svn_env()
+        _env_paths()
+    except:
+        print "Try committing local changes first."
